b39b6b6e332fb15531c9e1740a728265e8c05a10
Matthew Wolff <mm.wolff@chi-squared.org>
2020-01-08 14:32:00 +0000

    rewrote everything as object-oriented
    generally just cleaned everything up
    
    added colors lol

--- README.md
+++ README.md
@@ -2,59 +2,64 @@
 
 ## Example:
 
-Here I'll run the script two times in a row on myself
+I'll run the script two times in a row on myself, looking at 31 days of tweets at a time (I don't tweet that often). By default, the scraper will start whenever the user created their twitter. To save time in this scrape, I've told it to start at the beginning of 2019 and go up until the current date (you can change this using the `--until` flag).
 ```bash
-$ ./scrape.py -u thematthewwolff --since 2019-01-01
-scrape completed. found 12 tweets
-found 12 new tweets
-done.
+$ ./scrape.py -u realwoofy --since 2019-01-01 --by 31
+[ scraping user @realwoofy... ]
+[ found 0 existing tweets ]
+[ found 149 new tweets ]
+[ finished scraping ]
+[ stored tweets in realwoofy.json ]
 
-$ ./scrape.py -u thematthewwolff --since 2019-01-01
-scrape completed. found 12 tweets
-found 12 existing tweets
-found 0 new tweets
-done.
+$ ./scrape.py -u realwoofy --since 2019-01-01 --by 31
+[ scraping user @realwoofy... ]
+[ found 149 existing tweets ]
+[ found 0 new tweets ]
+[ finished scraping ]
+[ stored tweets in realwoofy.json ]
 ```
 
 ## Description
 
 Twitter's API limits you to querying a user's most recent 3200 tweets. This is a pain in the ass. However, we can circumvent this limit using Selenium and doing some webscraping.  
 
-We will query, month by month, a user's entire time on twitter. We'll collect the IDs of all their tweets—this is done within `scrape.py`. From there, we'll use the `tweepy` API to query the metadata associated with each tweet—this is done in `metadata.py`. You can adjust what metadata is collected in the same file by changing the variable `METADATA_LIST`. Personally, I was just building Markov models so I only cared about the `full_text` and if if was a retweet or not, but I've also included a list of all tweet attributes so that you can adjust it.
+We can query a user's entire time on twitter, finding the IDs for each of their tweets. From there, we can use the `tweepy` API to query the complete metadata associated with each tweet. You can adjust which metadata are collected by changing the variable `METADATA_LIST` at the top of `scrape.py`. Personally, I was just collecting text to train a model, so I only cared about the `full_text` field in addition to whether the tweet was a retweet.  
 
-## Requirements (loosely)
+I've included a list of all available tweet attributes so that you can adjust things as you wish.
 
-* python3 (3.6.5)
-* Modules (installed via pip3):
+## Requirements (or rather, what I used)
+
+* python3 (3.7.3)
+* Modules (via `pip`):
   * selenium (3.141.0)
-  * tweepy (3.7.0)
-  * requests (2.18.4)
-  * beautifulsoup4 (4.6.0)
-* Chrome webdriver (you can use other kinds. Personally, `brew install chromedriver`)
+  * tweepy (3.8.0)
+  * requests (2.21.0)
+  * beautifulsoup4 (4.7.1)
+* [Chrome webdriver](https://chromedriver.chromium.org/downloads) (you can use other kinds. Personally, `brew install chromedriver`)
 * [Twitter API developer credentials](https://dev.twitter.com)
 
 ## Using the Scraper
 
-* run `python3 scrape.py` and add the arguments you need
+* run `python3 scrape.py` and add the arguments you desire. Try `./scrape.py --help` for all options.
   * `-u` followed by the username [required]
-  * `--since` followed by a date string e.g (2017-01-01). Defaults to whenever the user created their twitter
-  * `--until` followed by a date string e.g (2018-01-01). Defaults to the current day 
-  * `--by` followed by the number of days to scrape at once (default: 1)
-    * If someone tweets a lot, keep this low
+  * `--since` followed by a date string, e.g., (2017-01-01). Defaults to whenever the user created their twitter
+  * `--until` followed by a date string, e.g., (2018-01-01). Defaults to the current day 
+  * `--by` followed by the number of days to scrape at once (default: 14)
+    * If someone tweets dozens of times a day, it might be better to use a lower number
   * `--delay` followed by an integer. This will be the number of seconds to wait on each page load before reading the page
-    * if your internet is slow, put this higher (default: 2)
-* a browser window will pop up and begin scraping month by month
-* when the browser window closes, metadata collections begins
-* when it finishes collection, it will dump all the data to a `.json` file that corresponds to the user
-  * Don't worry about time overlaps: if you run two overlapping time frames, it will only retrieve the unique tweets!
+    * if your internet is slow, put this higher (default: 2 seconds)
+* a browser window will pop up and begin scraping 
+* when the browser window closes, metadata collection begins for all new tweets
+* when collection finishes, it will dump all the data to a `.json` file that corresponds to the twitter handle
+  * don't worry about running two scrapes that have a time overlap; it will only retrieve new tweets!
 
 ## Troubleshooting
 
-* do you get a driver error when you try and run the script?
-  * open `scrape.py` and change the driver to use Chrome() or Firefox()
-    * if neither work, google the error (you probably need to install a new driver)
-* does it seem like it's not collecting tweets for days that have tweets?
-  * open `scrape.py` and change the delay variable to 2 or 3
+* do you get a driver error when you try and execute the script?
+  * make sure your browser is up to date and that you have a driver version that matches your browser version 
+  * you can also open `scrape.py` and change the driver to use Chrome() or Firefox()
+* does the scraper seem like it's missing tweets that you know should be there?
+  * try increasing the `--delay` parameter, it likely isn't waiting long enough for everything to load
 
 ## Twitter API credentials
 
--- scrape.py
+++ scrape.py
@@ -1,18 +1,180 @@
 #!/usr/bin/env python3
 
 import argparse
-import datetime
 import json
+from datetime import datetime, timedelta
+from functools import reduce
+from os import path
 from time import sleep
 
-import metadata
+import tweepy
 from bs4 import BeautifulSoup
 from requests import get
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
-from selenium.webdriver.common.keys import Keys
+
+from api_key import key
+
+######################### METADATA ATTRIBUTES ##########################
+# created_at, id, id_str, full_text, truncated, display_text_range,
+# entities, source, in_reply_to_status_id, in_reply_to_status_id_str,
+# in_reply_to_user_id, in_reply_to_user_id_str, in_reply_to_screen_name,
+# user, geo, coordinates, place, contributors, is_quote_status,
+# retweet_count, favorite_count, favorited, retweeted, lang
+########################################################################
+
+DATE_FORMAT = "%Y-%m-%d"
+METADATA_LIST = [  # note: "id" is already used later
+    "full_text",
+    "in_reply_to_status_id"
+]
+
+# colors for output
+RESET = "\033[0m"
+bw = lambda s: "\033[1m\033[37m" + s + RESET  # bold white
+w = lambda s: "\033[1m" + s + RESET  # white
+g = lambda s: "\033[32m" + s + RESET  # green
+y = lambda s: "\033[33m" + s + RESET  # yellow
 
 
+class Scraper:
+
+    def __init__(self, handle):
+        self.api = self.__authorize()
+        self.handle = handle
+        self.outfile = self.handle + ".json"
+        self.new_tweets = set()  # ids
+        self.tweets = self.__retrieve_existing()  # actual tweets
+
+    @staticmethod
+    def __authorize():
+        consumer_key = key["consumer_key"]
+        consumer_secret = key["consumer_secret"]
+        access_token = key["access_token"]
+        access_token_secret = key["access_token_secret"]
+
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+        return api
+
+    def __retrieve_existing(self):
+        tweets = dict()
+        if path.exists(self.outfile):
+            with open(self.outfile) as o:
+                tweets = json.load(o)
+        return tweets
+
+    def __check_if_scrapable(self):
+        try:
+            u = self.api.get_user(self.handle)
+            if not u.following and u.protected:
+                exit("Cannot scrape a private user unless this API account is following them.")
+        except tweepy.TweepError as e:
+            if e.api_code is 50:
+                exit("This user does not exist.")
+            else:
+                raise e
+
+    def scrape(self, start, end, by, loading_delay):
+        self.__check_if_scrapable()
+        print(bw("["), g("scraping user"), w("@") + y(self.handle) + g("..."), bw("]"))
+        print(bw("["), g("found"), w(str(len(self.tweets))), g("existing tweets"), bw("]"))
+        self.__find_tweets(start, end, by, loading_delay)
+        print(bw("["), g("found"), w(str(len(self.new_tweets))), g("new tweets"), bw("]"))
+        self.__retrieve_new_tweets()
+        print(bw("["), g("finished scraping"), bw("]"))
+
+    def __find_tweets(self, start, end, by, delay):
+        # gross CSS stuff -- don't touch
+        id_selector = ".time a.tweet-timestamp"
+        tweet_selector = "li.js-stream-item"
+
+        def slide(date, i):
+            return date + timedelta(days=i)
+
+        def form_url(start, end):
+            base_url = "https://twitter.com/search"
+            query = "?f=tweets&vertical=default&q=from%3A{}%20since%3A{}%20until%3A{}include%3Aretweets&src=typd"
+            return base_url + query.format(self.handle, start, end)
+
+        with webdriver.Chrome() as driver:  # options are Chrome(), Firefox(), Safari()
+            days = (end - start).days + 1
+
+            # scrape tweets using a sliding window
+	    window_start, ids = start, set()
+            for _ in range(days)[::by]:
+                # scrape the proper window of time for tweets (must format window beginning and end)
+                since = window_start.strftime(DATE_FORMAT)
+                until = slide(window_start, by).strftime(DATE_FORMAT)
+
+                # query tweets and slide window along
+                driver.get(form_url(since, until))
+                window_start = slide(window_start, by)
+
+                # load
+                sleep(delay)
+
+                try:
+                    found_tweets = driver.find_elements_by_css_selector(tweet_selector)
+                    increment = 10
+
+                    while len(found_tweets) >= increment:
+                        # print("scrolling down to load more tweets")
+                        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                        sleep(delay)
+                        found_tweets = driver.find_elements_by_css_selector(tweet_selector)
+                        increment += 10
+
+                    # print("{} tweets found, {} total".format(len(found_tweets), len(ids)))
+
+                    for tw in found_tweets:
+                        try:
+                            tweet_id = tw.find_element_by_css_selector(id_selector).get_attribute("href").split("/")[-1]
+                            ids.add(tweet_id)
+                        except StaleElementReferenceException:
+                            print("lost element reference", tw)
+                except NoSuchElementException:
+                    print("no tweets in time period {} -- {}".format(since, until))
+
+            self.new_tweets = ids - self.tweets.keys()  # remove known tweets from newly found tweets
+
+    def __retrieve_new_tweets(self):
+        tweets = self.__collect_new_tweet_metadata()
+        self.tweets.update(tweets)
+        self.new_tweets = set()  # reset
+
+    def __collect_new_tweet_metadata(self):
+        batch_size = 100  # http://docs.tweepy.org/en/v3.5.0/api.html#API.statuses_lookup
+        batch_indices = range(0, len(self.new_tweets), batch_size)
+        new_tweet_list = list(self.new_tweets)
+        batches = [new_tweet_list[i:i + batch_size] for i in batch_indices]
+
+        def extract_data(tweet_list):  # dictionary of id: metadata key-value pairs
+            return dict((tw["id"], {attr: tw[attr] for attr in METADATA_LIST}) for tw in tweet_list)
+
+        def staggered_lookup(id_batch):
+            queried_tweets = self.api.statuses_lookup(id_batch, tweet_mode='extended')
+            sleep(6)  # don't hit API rate limit
+            return extract_data(t._json for t in queried_tweets)
+
+        # collect all tweets as a list of dictionaries
+        tweet_batches = [staggered_lookup(batch) for batch in batches]
+
+        def dict_combiner(d1, d2): return d1.update(d2) or d1  # cheeky
+
+        # consolidate all tweet dictionaries
+        tweets = reduce(dict_combiner, tweet_batches) if len(tweet_batches) is not 0 else dict()
+        return tweets
+
+    def dump_tweets(self):
+        # write out
+        with open(self.outfile, "w") as o:
+            json.dump(self.tweets, o)
+        print(bw("["), g("stored tweets in"), y(self.outfile), bw("]"))
+
+
+# HELPER
 def get_join_date(handle):
     """
     Helper method - checks a user's twitter page for the date they joined
@@ -22,104 +184,22 @@ def get_join_date(handle):
     soup = BeautifulSoup(page, "html.parser")
     date_string = soup.find("span", {"class": "ProfileHeaderCard-joinDateText"})["title"].split(" - ")[1]
     date_string = "0" + date_string if date_string[1] is " " else date_string  # add on leading 0 if needed
-    return datetime.datetime.strptime(date_string, "%d %b %Y")
-
-
-def format_day(date):
-    day = "0" + str(date.day) if len(str(date.day)) is 1 else str(date.day)
-    month = "0" + str(date.month) if len(str(date.month)) is 1 else str(date.month)
-    year = str(date.year)
-    return "-".join([year, month, day])
-
-
-def form_url(user, start, end):
-    base_url = "https://twitter.com/search"
-    query = "?f=tweets&vertical=default&q=from%3A{}%20since%3A{}%20until%3A{}include%3Aretweets&src=typd"
-    return base_url + query.format(user, start, end)
-
-
-def increment_day(date, i):
-    return date + datetime.timedelta(days=i)
-
-
-def scrape(start, end, user, by, delay):
-    # no touchie
-    id_selector = ".time a.tweet-timestamp"
-    tweet_selector = "li.js-stream-item"
-
-    driver = webdriver.Chrome()  # options are Chrome() Firefox() Safari()
-    days = (end - start).days + 1
-    ids = []
-
-    for __ in range(days)[::by]:
-        d1 = format_day(increment_day(start, 0))
-        d2 = format_day(increment_day(start, by))
-        url = form_url(user, start=d1, end=d2)
-        driver.get(url)
-        sleep(delay)
-
-        try:
-            found_tweets = driver.find_elements_by_css_selector(tweet_selector)
-            increment = 10
-
-            while len(found_tweets) >= increment:
-                # print("scrolling down to load more tweets")
-                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                sleep(delay)
-                found_tweets = driver.find_elements_by_css_selector(tweet_selector)
-                increment += 10
-
-            # print("{} tweets found, {} total".format(len(found_tweets), len(ids)))
-
-            for tweet in found_tweets:
-                try:
-                    tweet_id = tweet.find_element_by_css_selector(id_selector).get_attribute("href").split("/")[-1]
-                    ids.append(tweet_id)
-                except StaleElementReferenceException:
-                    print("lost element reference", tweet)
-        except NoSuchElementException:
-            print("no tweets in this time period")
-
-        start = increment_day(start, by)
-
-    print("scrape completed. found", len(ids), "tweets")
-    driver.close()
-    return ids
-
-
-def dump_data(ids, twitter_ids_filename="all_ids.json"):
-    all_ids = set(ids)
-    try:  # combine old and new tweets
-        with open(twitter_ids_filename) as f:
-            old_ids = set(json.load(f))
-            print("new tweets found on this scrape:", len(old_ids - all_ids))
-            all_ids &= old_ids  # unique
-    except FileNotFoundError:
-        pass
-
-    with open(twitter_ids_filename, "w") as outfile:
-        data_to_write = list(all_ids)
-        json.dump(data_to_write, outfile)
-        print("total tweet count:", len(data_to_write))
+    return datetime.strptime(date_string, "%d %b %Y")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="scrape.py", usage="python3 %(prog)s [options]",
                                      description="scrape.py - Twitter Scraping Tool")
-    parser.add_argument("-u", help="Scrape this user's Tweets")
+    parser.add_argument("-u", "--username", help="Scrape this user's Tweets", required=True)
     parser.add_argument("--since", help="Get Tweets after this date (Example: 2010-01-01).")
     parser.add_argument("--until", help="Get Tweets before this date (Example: 2018-12-07.")
-    parser.add_argument("--by", help="Look at this many days at a time when scraping")
-    parser.add_argument("--delay", help="Time to wait in seconds on each page load before reading the page")
+    parser.add_argument("--by", help="Scrape this many days at a time", type=int, default=14)
+    parser.add_argument("--delay", help="Time given to load a page before scraping it (seconds)", type=int, default=2)
     args = parser.parse_args()
 
-    date_format = "%Y-%m-%d"
-    parse_date = datetime.datetime.strptime
-    username = args.u
-    begin = parse_date(args.since, date_format) if args.since is not None else get_join_date(username)
-    end = parse_date(args.until, date_format) if args.until is not None else datetime.datetime.now()
-    by = int(args.delay) if args.by is not None and args.by.isdigit() else 1
-    delay_time = int(args.delay) if args.delay is not None and args.delay.isdigit() else 2
+    begin = datetime.strptime(args.since, DATE_FORMAT) if args.since else get_join_date(args.username)
+    end = datetime.strptime(args.until, DATE_FORMAT) if args.until else datetime.now()
 
-    tweet_ids = scrape(begin, end, username, by, delay_time)
-    metadata.retrieve(outfile=args.u + ".json", tweet_ids=tweet_ids)
+    user = Scraper(args.username)
+    user.scrape(begin, end, args.by, args.delay)
+    user.dump_tweets()
